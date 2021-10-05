bccad5b9e223055c39bebb1cb53576a8236d7cde
Binghui Liu <006285600@coyote.csusb.edu>
2021-10-05 03:42:53 +0000

    Update bmi.page.ts

--- src/app/pages/bmi/bmi.page.ts
+++ src/app/pages/bmi/bmi.page.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BmiPage implements OnInit {
 
+  public height:any;
+  public weight:any;
   constructor() { }
 
   ngOnInit() {
   }
 
+  calculate(){
+    var kgweight = this.weight/2.205;
+    var BMI = (kgweight/(this.height * this.height)).toFixed(2);
+    let myContainer = document.getElementById('mybmi') as HTMLInputElement;
+    
+    return myContainer.innerHTML = BMI;;
+    }
 }
+
