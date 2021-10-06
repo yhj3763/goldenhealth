d7dcd2a5cf257ba9302b49f7e9e852044967658a
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2021-10-06 00:00:57 -0700

    Space for ts on environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,12 +7,12 @@ export const environment = {
   //this is my firebase api key 
   firebase : {
     apiKey: "AIzaSyBiwrmMOp-OQK-n4rHalA4ySk8As17N9Lk",
-  authDomain: "goldteamhealthproject.firebaseapp.com",
-  projectId: "goldteamhealthproject",
-  storageBucket: "goldteamhealthproject.appspot.com",
-  messagingSenderId: "647839527662",
-  appId: "1:647839527662:web:02d4897c9e7b124a8179ff",
-  measurementId: "G-V135VQ5542"}
+    authDomain: "goldteamhealthproject.firebaseapp.com",
+    projectId: "goldteamhealthproject",
+    storageBucket: "goldteamhealthproject.appspot.com",
+    messagingSenderId: "647839527662",
+    appId: "1:647839527662:web:02d4897c9e7b124a8179ff",
+    measurementId: "G-V135VQ5542"}
 };
 
 /*
