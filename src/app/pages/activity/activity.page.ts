a9cb959553d6a6196f60b1631afbac1c2239e6ad
Binghui Liu <006285600@coyote.csusb.edu>
2021-10-10 05:01:44 +0000

    Update activity.page.ts

--- src/app/pages/activity/activity.page.ts
+++ src/app/pages/activity/activity.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginPage } from 'src/app/login/login.page';
+import { FireserviceService } from '../../fireservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-activity',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity.page.scss'],
 })
 export class ActivityPage implements OnInit {
-
-  constructor() { }
+  private uid = this.logininfo.uid();
+  public Date:any;
+  public Workout_name: any;
+  public Set: any;
+  public Reps: any;
+  constructor(    public router:Router,
+    public fireService:FireserviceService, 
+    public logininfo: LoginPage  ) { }
 
   ngOnInit() {
   }
-
+  saveactivity(){
+    let data = {
+      Date:this.Date.split('T')[0],
+      Workout_name: this.Workout_name,
+      Set: this.Set,
+      Reps: this.Reps,
+      uid:this.uid
+    }
+    this.fireService.saveActivity(data).then(res=>{
+      console.log("Activity saved")
+      console.log(this.Date)
+      },err=>{
+        console.log(this.Date)
+        console.log(err);
+      });
+  }
 }
