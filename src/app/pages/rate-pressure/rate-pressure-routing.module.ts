2fa5d903015105b54ac0bb45a4fd316efd997b1e
Hyeongjun Yang <005897537@coyote.csusb.edu>
2021-10-19 21:13:05 +0000

    Update rate-pressure-routing.module.ts

--- src/app/pages/rate-pressure/rate-pressure-routing.module.ts
+++ src/app/pages/rate-pressure/rate-pressure-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { RatePressurePage } from './rate-pressure.page';
+import { RatePressurePage } from './rate-pressure.page'
 
 const routes: Routes = [
   {
