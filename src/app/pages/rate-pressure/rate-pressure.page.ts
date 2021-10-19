74743f48db7c2e15b4511ec4348481bcdfb891f4
Hyeongjun Yang <005897537@coyote.csusb.edu>
2021-10-19 21:15:47 +0000

    Update rate-pressure.page.ts

--- src/app/pages/rate-pressure/rate-pressure.page.ts
+++ src/app/pages/rate-pressure/rate-pressure.page.ts
@@ -1,15 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rate-pressure',
   templateUrl: './rate-pressure.page.html',
   styleUrls: ['./rate-pressure.page.scss'],
 })
-export class RatePressurePage implements OnInit {
+export class RatePressurePage {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {// Element declaration
+	const canvas = <HTMLCanvasElement> document.getElementById('canvas'); 
+	const colorR = document.getElementById('color-r');
+	const colorG = document.getElementById('color-g');
+	const colorB = document.getElementById('color-b');
+	const heartbeat = document.getElementById('heartbeat');
+	const beatLabel = document.getElementById('beat-label');
+	const stable = document.getElementById('stable');
+	const context = canvas.getContext('2d');
+	const video = document.getElementById('video') as HTMLVideoElement;
+
+	// Constants declaration
+	const itemsLimit = 100;
+	const beatsLimit = 15;
+	const lowerThreshold = 0.2;
+	const upperThreshold = 0.8;
+
+	const darkDetectionFactor = 5;
+
+	// Heartbeat buffer
+	const buffer = [];
+	const beats = [];
+	let stabilizingCount = 0;
+
+	// Initial flexible threshold
+	let threshold = upperThreshold;
+
+	// Content update for the webpage
+	function updateContent() {
+	context.drawImage(video, 0, 0, 300, 300);
+	const frame = context.getImageData(0, 0, 300, 300);
+	const length = frame.data.length / 4;
+	let [r, g, b] = [0, 0, 0];
+	for(let i = 0; i < length; i++) {
+		r = (i*r + frame.data[i*4+0])/(i+1);
+		g = (i*g + frame.data[i*4+1])/(i+1);
+		b = (i*b + frame.data[i*4+2])/(i+1);
+	}
+	colorR.innerHTML = `R: ${r.toFixed(3)}`;
+	colorG.innerHTML = `G: ${g.toFixed(3)}`;
+	colorB.innerHTML = `B: ${b.toFixed(3)}`;
+
+	// Light detection
+	if (b + g > 30) {
+		heartbeat.innerHTML = "";
+		beatLabel.innerHTML = '';
+		stabilizingCount = 0;
+		return;
+	}
+
+	// Dark detection
+	if (r < darkDetectionFactor * b) {
+		heartbeat.innerHTML = "";
+		beatLabel.innerHTML = '';
+		stabilizingCount = 0;
+		return;
+	}
+
+	// Heartbeat monitor
+	buffer.push(r);
+	if (buffer.length > itemsLimit) buffer.shift();
+
+	const maxRed = Math.max(...buffer);
+	const minRed = Math.min(...buffer);
+	const deltaRed = maxRed - minRed;
+
+	if ((threshold > 0.5) && (r > minRed + threshold * deltaRed)) {
+		beats.push(Date.now());
+		threshold = lowerThreshold;
+		beatLabel.innerHTML = '';
+		stabilizingCount++;
+	} else if ((threshold < 0.5) && (r < minRed + threshold * deltaRed)) {
+		beats.push(Date.now());
+		threshold = upperThreshold;
+		beatLabel.innerHTML = '';
+		stabilizingCount++;
+	} else return;
+
+	if (beats.length > beatsLimit) beats.shift();
+
+	const intervals = [];
+	for (let i = 1; i < beats.length; i++) intervals.push(beats[i] - beats[i-1]);
+
+	const heartMeasure = 30000 / (intervals.reduce((a, b) => a + b, 0) / intervals.length);
+	heartbeat.innerHTML = (stabilizingCount > beatsLimit)
+		? ` ${heartMeasure.toFixed(0)}`
+		:  ``
+	stable.innerHTML =(stabilizingCount > beatsLimit)
+	    ? ``
+	    :`Please hold the camera. <br/> Value is stabilized: ${stabilizingCount}/${beatsLimit}`
+	}
+	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+	navigator.mediaDevices.getUserMedia({ video:{facingMode: 'enviornment'}} ).then(function(stream) {
+	video.srcObject = stream;
+	video.play();
+	});
+	}
+
+	setInterval(updateContent, 0);
+
+  }
 }
