934d14fe691d8b9d854d4f3503c0977d4ad60e60
Binghui Liu <006285600@coyote.csusb.edu>
2021-10-20 06:59:38 +0000

    Update home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { LoginPage } from '../login/login.page';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [LoginPage]
 })
 export class HomePageModule {}
