b49d77179d9c2e8e820c64f35174d98da366e0b8
Binghui Liu <006285600@coyote.csusb.edu>
2021-10-20 06:59:05 +0000

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginPage } from 'src/app/login/login.page';
+import { AngularFirestore } from "@angular/fire/compat/firestore"; //import the firestore database
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { FireserviceService } from '../fireservice.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  private uid = this.logininfo.uid();
+  users: Observable<any>;
+  constructor(
+    public router:Router,
+    public fireService:FireserviceService, 
+    public logininfo: LoginPage,
+    public firestore: AngularFirestore,
+  ) {}
+  ngOnInit() {
+    this.users = this.firestore.collection("users").doc(this.uid).collection("PersonalInfo").valueChanges();
 
-  constructor() {}
-
+  }
+  
 }
